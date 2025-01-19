c517d5ba29ecd28f1d57a938406f24f0a28a5f8b
begizew <begizewzerihun@gmail.com>
2025-01-19 16:17:48 -0500

    data sraping scripts

--- news_scraper/news_scraper/items.py
+++ news_scraper/news_scraper/items.py
@@ -11,14 +11,13 @@ class ArticleItem(scrapy.Item):
 # ...
 # //*[@id="lists"]/li[50]
 
-/html/body/div[2]/div/div[1]/div/h1
+#/html/body/div[2]/div/div[1]/div/h1
 # path of u 
 # /html/body/div[2]/div/div[1]/div/h1...title
 # /html/body/div[2]/div/div[1]/div/div[2]/div/ul/li[1].. view
 # /html/body/div[2]/div/div[1]/div/div[2]/div/ul/li[2]..date
 # /html/body/div[2]/div/div[1]/div/div[2]/div/ul/li[3]..source
 # /html/body/div[2]/div/div[1]/div/div[2]/article .. article
-
 
 # //*[@id="lists"]/li[8] <l>
 # /html/body/div[2]/div/div[1]/div/h1 ...title
@@ -34,6 +33,6 @@ class ArticleItem(scrapy.Item):
 # //*[@id="lists"]/li[7]/div[1]/h4
 # //*[@id="lists"]/li[8]/div[1]/h4
 
-//*[@id="lists"]/li[7]/div[1]/h4/a
-//*[@id="lists"]/li[8]/div[1]/h4/a
-//*[@id="lists"]/li[7]/div[1]/h4/a
+# //*[@id="lists"]/li[7]/div[1]/h4/a
+# //*[@id="lists"]/li[8]/div[1]/h4/a
+# //*[@id="lists"]/li[7]/div[1]/h4/a
