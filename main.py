1a86dc6c04d0c17103438be647c929da7e8f54b8
Begizew2112 <begizewzerihun3@gmail.com>
2025-02-06 00:00:07 -0500

    update

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from scrapers.dealstreet_scraper import DealStreetScraper
+from scrapers.dealstreet_venture_capital import DealStreetScraper
 from scrapers.private_equity_scraper import PrivateEquityScraper
 from scrapers.fromgeek_scraper import FromGeekScraper
-from scrapers.techstars_scraper import TechStarsScraper  # New scraper added
+from scrapers.techstars_scraper import TechStarsScraper
+from scrapers.vccircle_pe_scraper import VCCirclePEScraper  # New scraper added
+from scrapers.vccircle_venture_capital import VCCirclesVentureCapital# New scraper added
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 # Choose which scraper to run
-SCRAPER_TO_RUN = "techstars"  # Change to "dealstreet", "private_equity", "fromgeek", or "techstars" as needed
+SCRAPER_TO_RUN = "techstars"  # Change as needed
 
 if __name__ == "__main__":
     if SCRAPER_TO_RUN == "dealstreet":
@@ -12,17 +17,19 @@ if __name__ == "__main__":
     elif SCRAPER_TO_RUN == "private_equity":
         scraper = PrivateEquityScraper(headless=True)
     elif SCRAPER_TO_RUN == "fromgeek":
-        scraper = FromGeekScraper(headless=True)
-    elif SCRAPER_TO_RUN == "techstars":  # New option for TechStars
-        scraper = TechStarsScraper(headless=True)
+        scraper = FromGeekScraper(headless= True)
+    elif SCRAPER_TO_RUN == "techstars":
+        scraper = TechStarsScraper(headless=False)
+    elif SCRAPER_TO_RUN == "vccircle_pe":  # New option for VCCircle PE
+        scraper = VCCirclePEScraper(headless=True)
+    elif SCRAPER_TO_RUN == "vccircle_capital":  # New option for VCCircle PE
+        scraper = VCCirclesVentureCapital(headless=True)
     else:
         raise ValueError("Invalid scraper selection!")
-
+    # Start scraping the selected scraper
     scraper.scrape_articles()
 
-
 #from scrapers.fromgeek_scraper import FromGeekScraper
-
 # # Scraper for French to English
 # scraper = FromGeekScraper(headless=False)
 # scraper.scrape()
