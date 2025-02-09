76721f786f8b9e97d0765c9435b2bb8784ebf18e
Begizew2112 <begizewzerihun3@gmail.com>
2025-02-08 19:28:32 -0500

    rearrange

--- my_scraper/check.py
+++
@@ -1,7 +0,0 @@
-# https://www.vccircle.com/deal-type/pe.first page 
-# https://www.vccircle.com/deal-type/pe/all/2..second page
-# //*[@id="__next"]/div/div/div/div[2]/div[1]/div[4]/div.. article 
-# //*[@id="__next"]/div/div/div/div[2]/div[1]/div[4]/div/div/div[2]/h4/a..article url 
-# https://www.vccircle.com/deal-type/pe/all/3..third page 
-# https://www.vccircle.com/deal-type/pe/all/4
-# https://www.vccircle.com/deal-type/pe/all/5
