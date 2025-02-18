c1403b2320960723ad0bdc6c284cac1e425a0a9e
Begizew2112 <begizewzerihun3@gmail.com>
2025-02-17 21:30:00 -0500

    update

--- my_scraper/scrapers/base_scraper.py
+++ my_scraper/scrapers/base_scraper.py
@@ -5,7 +5,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 from webdriver_manager.chrome import ChromeDriverManager
 from utils.selenium_utils import configure_translation_options
 import time
